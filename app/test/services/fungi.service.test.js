0fdc426eb7aaa473a10a8ce5b8e2452273e93ffe
blueberry <jnb@campus.uni-paderborn.de>
2024-12-18 21:06:09 +0100

    test: test validate static rule system

--- app/test/services/fungi.service.test.js
+++ app/test/services/fungi.service.test.js
@@ -7,11 +7,7 @@ FUNGISTART ONREPLY "Hello" DORESPOND "Hello, Fediverse user!"; FUNGIEND
 `;
 
 describe('Test parser', function(){
-    it('process code 1 correctly', function(){
-        const tokens = ruleParser.tokenize(testCode1);
-        const commands = ruleParser.parse(tokens);
-        const reply = ruleParser.calculateResponse(commands, "Hello");
-
-        assert.equal(reply, "Hello, Fediverse user!");
+    it('validate for FUNGI static rule system', function(){
+        assert.isFalse(ruleParser.containsValidFUNGI(testCode1));
     });
 });
